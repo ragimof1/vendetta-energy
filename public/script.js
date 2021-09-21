3afe48ea189faa240a5b786be1d1be9666c8c175
Ilkin Ragimov <rehimilkin346@gmail.com>
2021-09-22 00:39:11 +0400

    Added form

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-ScrollReveal().reveal('#products',{delay: 400})
+ScrollReveal().reveal('.products',{delay: 400})
+ScrollReveal().reveal('.energy-container',{delay: 400})
+ScrollReveal().reveal('.energy-container-p',{delay: 400})
+ScrollReveal().reveal('.order',{delay: 400})
+ScrollReveal().reveal('.form',{delay: 400})
+ScrollReveal().reveal('.flex',{delay: 400})
+ScrollReveal().reveal('.energy-container',{delay: 400})
+ScrollReveal().reveal('.btn',{delay: 400})
+const btn = document.querySelector('.send')
+btn.addEventListener('click', () => {
+    alert("This service is unavailable")
+})
