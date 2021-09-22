6f41f04ebe9cc1c26cc1703226981b1a6f9ed6a7
Ilkin Ragimov <rehimilkin346@gmail.com>
2021-09-22 23:05:42 +0400

    Added about

--- public/script.js
+++ public/script.js
@@ -6,6 +6,8 @@ ScrollReveal().reveal('.form',{delay: 400})
 ScrollReveal().reveal('.flex',{delay: 400})
 ScrollReveal().reveal('.energy-container',{delay: 400})
 ScrollReveal().reveal('.btn',{delay: 400})
+ScrollReveal().reveal('.about',{delay: 400})
+ScrollReveal().reveal('.cards',{delay: 400})
 const btn = document.querySelector('.send')
 btn.addEventListener('click', () => {
     alert("This service is unavailable")
